ef3f4738c8ec102dcc386429afbbbe64e7da2762
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-11-12 19:39:20 -0600

    modified facebook

--- facebook.js
+++ facebook.js
@@ -1,41 +1,40 @@
+var	accessToken = 'EAACEdEose0cBAOip5gPZAaSZCOAeZAXf2CXThZBJTT6SISaBqfGBiqs4g7A7TxN3jpMlYkCEh7bnSFJPNsz5kI97pdnyFqWCt6V2eRumczZAdmSVVfKzxJOrlw9183mnI2qBYm0B2DfRgODMZB8Uo8ZCfBVZAZAkyW0FkzZA0IEqjvNAZDZD'
+const RapidAPI = new require('rapidapi-connect');
+const rapid = new RapidAPI('JiboUp', '9bb11e09-8d1f-41b8-8901-6871186bf5eb');
+
 function getPostsOfUser(accessToken) {
+	
 	return new Promise((resolve, reject) => {
 
-	const RapidAPI = new require('rapidapi-connect');
-	const rapid = new RapidAPI('JiboUp', '9bb11e09-8d1f-41b8-8901-6871186bf5eb');
-
-	function getTimeline(accessToken) {
-		return new Promise((resolve, reject) => {
-			rapid.call('FacebookGraphAPI', 'getUsersFeed', { 
-				'access_token': accessToken,
-				'user_id': ''
-			}).on('success', (payload)=>{
-				console.log("yeh success")
-				resolve(payload)
-			}).on('error', (payload)=>{
-				console.log("o no failed")
-				reject(payload)
-			})
-		})
-	}
-
 	posts = []
-	getTimeline(accessToken).then(
-		function(timeline){
-			timelineJson = JSON.parse(timeline).data
-			resolve(
-				timelineJson.filter( 
-					(post) => post.message
-				).map(
-					(post) => post.message
-		))}
-	)
+	getTimeline(accessToken)
+	.then( (timeline) => {
+		timelineJson = JSON.parse(timeline).data
+
+		// filter out non mess and use mess
+		resolve( timelineJson
+					.filter((post) => post.message)
+					.map((post) => post.message)
+		)
+
+	}).catch( (error) => {
+		console.log(error);
+	});
 })}
 
-var	accessToken = 'EAACEdEose0cBAOip5gPZAaSZCOAeZAXf2CXThZBJTT6SISaBqfGBiqs4g7A7TxN3jpMlYkCEh7bnSFJPNsz5kI97pdnyFqWCt6V2eRumczZAdmSVVfKzxJOrlw9183mnI2qBYm0B2DfRgODMZB8Uo8ZCfBVZAZAkyW0FkzZA0IEqjvNAZDZD'
+function getTimeline(accessToken) {
+	return new Promise((resolve, reject) => {
+		rapid.call('FacebookGraphAPI', 'getUsersFeed', { 
+			'access_token': accessToken,
+			'user_id': ''
+		}).on('success', (payload)=>{
+			console.log("yeh success")
+			resolve(payload)
+		}).on('error', (payload)=>{
+			console.log("o no failed")
+			reject(payload)
+		})
+	})
+}
 
 getPostsOfUser(accessToken).then((data) => console.log(data));
-
-function getImagesOfUser{
-
-}
