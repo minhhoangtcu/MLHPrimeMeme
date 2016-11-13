42b2ecc53316b707f74d4164c2fd55f429ab1cc7
antonxuanquang <antonxuanquang@yahoo.com>
2016-11-13 07:43:16 -0600

    unknown error, push again

--- facebook-collect.js
+++ facebook-collect.js
@@ -1,12 +1,13 @@
 const FB = require('fb');
 
-var	accessToken = 'EAACEdEose0cBAJAe6KCTJ0nL73NnDJhMXSk8ZCSZAE0RdemwQaOw4wIMZBWMIIoQy8vqdQthdVeUvzWH69lNkJZAZBHJkS7R86cP6eZCRZAB8lp72osQrxNEjBZCKA6ZA0omzpUwhVUphzvbdriLRkDEraNpKq3O9rRFMU1BMVaymIQZDZD'
+// var	accessToken = 'EAACEdEose0cBAJAe6KCTJ0nL73NnDJhMXSk8ZCSZAE0RdemwQaOw4wIMZBWMIIoQy8vqdQthdVeUvzWH69lNkJZAZBHJkS7R86cP6eZCRZAB8lp72osQrxNEjBZCKA6ZA0omzpUwhVUphzvbdriLRkDEraNpKq3O9rRFMU1BMVaymIQZDZD'
 
 function getPhotosLink (accessToken, size) {
 	return new Promise((resolve, reject) => {
-		getPhotosID(accessToken, size).then((ids) => {
+		getPhotoIDs(accessToken, size).then((ids) => {
 			var promises = []
 			ids.forEach((id) => {
+				console.log(id)
 				promises.push(getAPhoto(accessToken, id))
 			})
 			Promise.all(promises).then((data) => {
@@ -18,7 +19,7 @@ function getPhotosLink (accessToken, size) {
 	})
 }
 
-function getPhotosID(accessToken, size) {
+function getPhotoIDs(accessToken, size) {
 	return new Promise((resolve, reject) => {
 		FB.options({accessToken: accessToken});
 		FB.api(
@@ -45,8 +46,8 @@ function getAPhoto(accessToken, id) {
 		FB.api(
 			id,
 			function (response) {
+				console.log(response)
 				if (response && !response.error) {
-					// console.log(response.images[0].source)
 					resolve(response.images[0].source);
 				} else {
 					reject(response.error);
@@ -85,5 +86,5 @@ exports.getPhotosLink = getPhotosLink;
 
 // getPhotosLink(accessToken, 10).then((data) => console.log(data));
 // getPhotosID(accessToken, 10).then((data) => console.log(data));
-// getAPhoto(accessToken, "1054981821244385").then((data) => console.log(data));
+getAPhoto(accessToken, "1054981821244385").then((data) => console.log(data));
 // getPosts(accessToken, 10).then((data) => console.log(data));
