0d2a8f7411501f843a3b0fdf3164ee630c4d2d87
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-11-13 07:03:39 -0600

    added jokes

---
+++ jokes.js
@@ -0,0 +1,33 @@
+var request = require('request');
+
+function getNorrisJoke(){
+	return new Promise((resolve, reject) => {
+		request('http://api.icndb.com/jokes/random?firstName=Jibo&amp;lastName=Doe', function (error, response, body) {
+			if (!error && response.statusCode == 200) 
+				resolve(JSON.parse(body).value.joke)
+			else if(error)
+				reject(error)
+		})
+	})
+}
+
+function getYoMama(){
+	return new Promise((resolve, reject) => {
+		request('http://api.yomomma.info/', function (error, response, body) {
+			if (!error && response.statusCode == 200) 
+				resolve(JSON.parse(body).joke)
+			else if(error)
+				reject(error)
+		})
+	})
+}
+
+function getJoke(){
+	m = Math.random()
+	if(m > 0.75)
+		return getNorrisJoke()
+	else
+		return getYoMama()
+}
+
+exports.getJoke = getJoke;
