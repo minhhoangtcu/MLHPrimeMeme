1233ae86164d91a64066d59162bb0d6e078d191c
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-11-15 17:02:46 -0600

    converted everything into oo

--- collect/facebook-collect.js
+++ collect/facebook-collect.js
@@ -7,7 +7,8 @@ class CollectFacebook {
   }
 
   /**
-  * Get the url and created time of a photo provided its id (Node).
+  * Get the url and created time of a photo provided its id (Node). You should never use this,
+  * because this is just the helper function for getPhotosInfo
   *
   * @return: {link: url, time: time} for time in the format similarly to this: 2016-11-11T18:54:36+0000
   */
@@ -30,81 +31,77 @@ class CollectFacebook {
       );
     });
   }
-}
 
-/**
- * Get the first n info of user's photos. For n be determined by 'size'
- */
-function getPhotosInfo (accessToken, size) {
-  return new Promise((resolve, reject) => {
-    getPhotoIDs(accessToken, size).then((ids) => {
-
-      let promises = []
-
-      ids.forEach((id) => {
-        let temp = getAPhoto(accessToken, id);
-        promises.push(temp)
-      })
-
-      Promise
-        .all(promises)
-        .then((data) => {
-          resolve(data)
-        })
-        .catch((error) => {
-          reject(error)
-        })
+  /**
+   * Get a certain number of photos IDs of the user. You should never use this, 
+   * because this is just the helper function for getPhotosInfo.
+   */
+  getPhotoIDs(size) {
+    return new Promise((resolve, reject) => {
+      FB.api(
+        '/me',
+        'GET',
+        {"fields":`photos.limit(${size}{id})`},
+        (response) => {
+          if (response && !response.error) {
+            resolve(
+              response.photos.data
+                .map((image) => image.id)
+            );
+          } else {
+            reject(response.error);
+          }
+        }
+      );
     })
-  })
-}
+  }
 
-/**
- * Get a certain number of photos IDs of the user.
- */
-function getPhotoIDs(accessToken, size) {
-  return new Promise((resolve, reject) => {
-    FB.options({accessToken: accessToken});
-    FB.api(
-      '/me',
-      'GET',
-      {"fields":`photos.limit(${size}{id})`},
-      (response) => {
-        if (response && !response.error) {
-          resolve(
-            response.photos.data
-              .map((image) => image.id)
-          );
-        } else {
-          reject(response.error);
+  /**
+   * Get the first n info of user's photos. For n be determined by 'size'
+   */
+  getPhotosInfo(size) {
+    return new Promise((resolve, reject) => {
+      this.getPhotoIDs(size).then((ids) => {
+
+        let promises = []
+
+        ids.forEach((id) => {
+          let temp = this.getAPhoto(id);
+          promises.push(temp)
+        })
+
+        Promise
+          .all(promises)
+          .then((data) => {
+            resolve(data)
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      })
+    })
+  }
+
+  getPostsOfUser(size) {
+    return new Promise((resolve, reject) => {
+      FB.api(
+        '/me',
+        'GET',
+        {"fields":`id,name,posts.limit(${size})`},
+        function(response) {
+          if (!response || response.error) {
+            reject(response.error);
+          } else {
+            resolve(
+              response.posts.data
+              .filter( (post) => post.message)
+              .map((post) => post.message)
+            )
+          }
         }
-      }
-    );
-  })
+      );
+    })
+  }
 }
 
-function getPostsOfUser (accessToken, size) {
-  return new Promise((resolve, reject) => {
-    FB.options({accessToken: accessToken});
-    FB.api(
-      '/me',
-      'GET',
-      {"fields":`id,name,posts.limit(${size})`},
-      function(response) {
-        if (!response || response.error) {
-          reject(response.error);
-        } else {
-          resolve(
-            response.posts.data
-            .filter( (post) => post.message)
-            .map((post) => post.message)
-          )
-        }
-      }
-    );
-  })
-}
-
-exports.getPostsOfUser = getPostsOfUser;
-exports.getPhotosInfo = getPhotosInfo;
-exports.getPhotoIDs = getPhotoIDs;
 module.exports = CollectFacebook;
