de08afd938dd427f4ca4194cba8ddd5b8c97ad8b
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-11-15 12:59:30 -0600

    fixed Facebook image collecting process

--- collect/facebook-collect.js
+++ collect/facebook-collect.js
@@ -1,83 +1,104 @@
 const FB = require('fb');
 
-function getPhotosLink (accessToken, size) {
-	return new Promise((resolve, reject) => {
-		getPhotoIDs(accessToken, size).then((ids) => {
-			var promises = []
-			ids.forEach((id) => {
-				promises.push(getAPhoto(accessToken, id)
-				// .then((data) => {
-				// 	console.log(data)
-				// })
-				)
-			})
-			Promise.all(promises).then((data) => {
-				resolve(data)
-			}).catch((error) => {
-				reject(error)
-			})
-		})
-	})
+/**
+ * Get the first n info of user's photos. For n be determined by 'size'
+ */
+function getPhotosInfo (accessToken, size) {
+  return new Promise((resolve, reject) => {
+    getPhotoIDs(accessToken, size).then((ids) => {
+
+      let promises = []
+
+      ids.forEach((id) => {
+        let temp = getAPhoto(accessToken, id);
+        promises.push(temp)
+      })
+
+      Promise
+        .all(promises)
+        .then((data) => {
+          resolve(data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  })
 }
 
+/**
+ * Get a certain number of photos IDs of the user.
+ */
 function getPhotoIDs(accessToken, size) {
-	return new Promise((resolve, reject) => {
-		FB.options({accessToken: accessToken});
-		FB.api(
-			'/me',
-			'GET',
-			{"fields":`photos.limit(${size}){id}`},
-			function(response) {
-				if (response && !response.error) {
-					resolve(
-						response.photos.data
-						.map((image) => image.id)
-					);
-				} else {
-					reject(response.error);
-				}
-			}
-		);
-	})
+  return new Promise((resolve, reject) => {
+    FB.options({accessToken: accessToken});
+    FB.api(
+      '/me',
+      'GET',
+      {"fields":`photos.limit(${size}{id})`},
+      (response) => {
+        if (response && !response.error) {
+          resolve(
+            response.photos.data
+              .map((image) => image.id)
+          );
+        } else {
+          reject(response.error);
+        }
+      }
+    );
+  })
 }
 
+/**
+ * Get the url and created time of a photo provided its id (Node).
+ *
+ * @return: {link: url, time: time} for time in the format similarly to this: 2016-11-11T18:54:36+0000
+ */
 function getAPhoto(accessToken, id) {
-	return new Promise((resolve, reject) => {
-		FB.options({accessToken: accessToken});
-		FB.api(
-			id,
-			function (response) {
-				if (response && !response.error) {
-					resolve(response.images[0].source);
-				} else {
-					reject(response.error);
-				}
-			}
-		);
-	})	
+  return new Promise((resolve, reject) => {
+    FB.options({accessToken: accessToken});
+    FB.api(
+      id,
+      'GET',
+      {'fields': 'images,created_time'},
+      (response) => {
+        if (response && !response.error) {
+          resolve(
+            {link: response.images[0].source,
+             time: response.created_time}
+          );
+        } else {
+          reject(response.error);
+        }
+      }
+    );
+  })  
 }
 
 function getPostsOfUser (accessToken, size) {
-	return new Promise((resolve, reject) => {
-		FB.options({accessToken: accessToken});
-		FB.api(
-			'/me',
-			'GET',
-			{"fields":`id,name,posts.limit(${size})`},
-			function(response) {
-				if (!response || response.error) {
-					reject(response.error);
-				} else {
-					resolve(
-						response.posts.data
-						.filter( (post) => post.message)
-						.map((post) => post.message)
-					)
-				}
-			}
-		);
-	})
+  return new Promise((resolve, reject) => {
+    FB.options({accessToken: accessToken});
+    FB.api(
+      '/me',
+      'GET',
+      {"fields":`id,name,posts.limit(${size})`},
+      function(response) {
+        if (!response || response.error) {
+          reject(response.error);
+        } else {
+          resolve(
+            response.posts.data
+            .filter( (post) => post.message)
+            .map((post) => post.message)
+          )
+        }
+      }
+    );
+  })
 }
 
 exports.getPostsOfUser = getPostsOfUser;
-exports.getPhotosLink = getPhotosLink;
+exports.getPhotosInfo = getPhotosInfo;
+exports.getPhotoIDs = getPhotoIDs;
+exports.getAPhoto = getAPhoto;
--- collect/facebookConfig.js
+++
@@ -1,6 +0,0 @@
-const facebookConfig = {
-    scope:      'email, user_about_me, user_birthday, user_location, user_photos, public_profile, user_posts, user_videos',
-    redirect_uri:   'https://d382144b.ngrok.io/auth/facebook'
-}
-
-exports.facebookConfig = facebookConfig
