fe6112b2bdc81cc4e978da6d540981ca106e28d7
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-11-15 21:34:01 -0600

    removed facebook functions (not needed)

--- collect/facebook-collect.js
+++ collect/facebook-collect.js
@@ -1,5 +1,4 @@
 const FB = require('fb');
-const csvGenerator = require('./csv-generator.js');
 
 class CollectFacebook {
 
@@ -58,61 +57,11 @@ class CollectFacebook {
   }
 
   /**
-   * Get all photos IDs of the user. You should never use this, 
-   * because this is just the helper function for getPhotosInfo.
-   */
-  getPhotoIDs() {
-    return new Promise((resolve, reject) => {
-      FB.api(
-        '/me',
-        'GET',
-        {"fields":`photos.limit({id})`},
-        (response) => {
-          if (response && !response.error) {
-            resolve(
-              response.photos.data
-                .map((image) => image.id)
-            );
-          } else {
-            reject(response.error);
-          }
-        }
-      );
-    })
-  }
-
-  /**
    * Get the first n info of user's photos. For n be determined by 'size'
    */
   getPhotosInfo(size) {
     return new Promise((resolve, reject) => {
       this.getPhotoIDs(size).then((ids) => {
-
-        let promises = []
-
-        ids.forEach((id) => {
-          let temp = this.getAPhoto(id);
-          promises.push(temp)
-        })
-
-        Promise
-          .all(promises)
-          .then((data) => {
-            resolve(data)
-          })
-          .catch((error) => {
-            reject(error)
-          })
-      })
-    })
-  }
-
-  /**
-   * Get the first n info of user's photos. For n be determined by 'size'
-   */
-  getPhotosInfo() {
-    return new Promise((resolve, reject) => {
-      this.getPhotoIDs().then((ids) => {
 
         let promises = []
 
@@ -145,29 +94,8 @@ class CollectFacebook {
           } else {
             resolve(
               response.posts.data
-              .filter( (post) => post.message)
-              .map((post) => post.message)
-            )
-          }
-        }
-      );
-    })
-  }
-
-  getPostsOfUser() {
-    return new Promise((resolve, reject) => {
-      FB.api(
-        '/me',
-        'GET',
-        {"fields":`id,name,posts`},
-        function(response) {
-          if (!response || response.error) {
-            reject(response.error);
-          } else {
-            resolve(
-              response.posts.data
-              .filter( (post) => post.message)
-              .map((post) => post.message)
+                .filter( (post) => post.message)
+                .map((post) => post.message)
             )
           }
         }
--- collect/usage-facebook.js
+++ collect/usage-facebook.js
@@ -7,10 +7,16 @@ collect.getPhotosInfo(5)
   .then((result) => {
     console.log('\n> Get Photos Info\n');
     console.log(result);
+  })
+  .catch((error) => {
+  	console.log(error);
   });
 
 collect.getPostsOfUser(5)
   .then((result) => {
     console.log('\n> Get Status Updates Info\n');
     console.log(result);
+  })
+  .catch((error) => {
+  	console.log(error);
   });
