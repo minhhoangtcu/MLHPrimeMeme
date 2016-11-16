ea556b9ae9263224a0dd0a435a6f39cf8975063a
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-11-15 21:31:56 -0600

    added a way to genearte an array of sentiment

--- processing_texts/alchemy.js
+++ processing_texts/alchemy.js
@@ -10,12 +10,34 @@ class TextEmotion {
     return new Promise((resolve, reject) => {
       var parameters = {text}
 
-      this.alchemy_language.emotion(parameters, function (err, response) {
+      this.alchemy_language.emotion(parameters, (err, response) => {
         if (err)
           reject(err);
         else
-          resolve(response.docEmotions, null, 2);
+          resolve(response.docEmotions);
       });
+    });
+  }
+
+  getAverageEmotionFromAll(arrayOfTexts) {
+    return new Promise( (resolve, reject) => {
+
+      var sentiments = [];
+
+      arrayOfTexts.forEach((text) => {
+        sentiments.push(this.getEmotion(text));
+      });
+
+      Promise
+        .all(sentiments) // after this we get an array of JSON
+        .then((data) => {
+          let sum = data.map((sentiment) => sentiment.joy)
+          resolve(sum);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+
     });
   }
 
