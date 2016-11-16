417319365b8ab76fd74a5e115aac50cc9567a4b8
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-11-15 21:12:24 -0600

    added a way to generate csv

---
+++ collect/csv-generator.js
@@ -0,0 +1,27 @@
+const stringify = require('csv-stringify');
+const fs = require('fs');
+
+/**
+ * Generate a CSV file provided the data formatted as an array of dictionary.
+ */
+function generateCSV(fileName, data) {
+  // Get headers
+  let columns = [];
+  Object.keys(data[0]).forEach((key) => {
+    columns.push(key);
+  });
+
+  // Convert the data into a string
+  stringify(data, {header: true, columns: columns}, (err, output) => {
+
+    // Write out the string into csv file
+    fs.writeFile(fileName + '.csv', output, (err) => {
+      if (err) throw err;
+      console.log('file saved');
+    });
+  });
+}
+
+// Usage
+// Must input an array of dictionary.
+generateCSV('test', [{a: 1, b: 2}, {a: 5, b: 23}]);
